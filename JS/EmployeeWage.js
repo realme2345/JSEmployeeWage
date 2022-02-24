f092537e2c215a73b1ebb6050b1bffec9999a2f1
unknown <rajakongara2507@gmail.com>
2022-02-24 19:24:13 +0530

    [Raja}Add To calculate the Employee Wage the Employee is present or Absent

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -1,9 +1,22 @@
-//UC1 Employee is present or Absent
-let Is_Absent=0;
-let empCheck=Math.floor(Math.random()*10)%2;
-if(empCheck==Is_Absent){
-    console.log("Employee is absent");
-}else{
-    console.log("Employee is Present");
+//Uc2 Ability to calculate the Daily wage
+const Is_FullTime=1;
+const Is_PartTime=2;
+const Working_Per_Hr=20;
+let totalEmployeeWage=0;
+let empHr=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck)
+{
+    case Is_FullTime:
+        empHr=8;
+        break;
+    case Is_PartTime:
+        empHr=4;
+        break;
+    default:
+        empHr=0;
+        break;
 }
+totalEmployeeWage=Working_Per_Hr*empHr;
+console.log("Totala Employee Wage :" + totalEmployeeWage);    
 
