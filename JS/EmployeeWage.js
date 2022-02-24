c05893615098b5d848b126cdd1bc6cbed209e767
unknown <rajakongara2507@gmail.com>
2022-02-24 17:30:20 +0530

    [Raja}Add To know the Employee is present or Absent

---
+++ JS/EmployeeWage.js
@@ -0,0 +1,9 @@
+//UC1 Employee is present or Absent
+let Is_Absent=0;
+let empCheck=Math.floor(Math.random()*10)%2;
+if(empCheck==Is_Absent){
+    console.log("Employee is absent");
+}else{
+    console.log("Employee is Present");
+}
+
