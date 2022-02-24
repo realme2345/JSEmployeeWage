47f8725f0b0d42cc017a066a25fc1114da644d38
unknown <rajakongara2507@gmail.com>
2022-02-24 21:35:36 +0530

    [Raja}Add To calculate the Employee Wage and EmpHr by using the function

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -1,4 +1,4 @@
-//Uc3 Ability to calculate the Daily wage using the function
+//Uc4 Ability to calculate the Daily wage and EmpHr using the function
 const Is_FullTime=1;
 const Is_PartTime=2;
 const Part_Time_Hr=4;
@@ -6,7 +6,6 @@ const Full_Time_Hr=8;
 const Working_Per_Hr=20;
 let totalEmployeeWage=0;
 let empHr=0;
-let empCheck=Math.floor(Math.random()*10)%3;
 function getWorkingHr(empCheck)
 {
     switch(empCheck)
@@ -19,7 +18,12 @@ function getWorkingHr(empCheck)
             return 0;
     }
 }
-empHr=getWorkingHr(empCheck);
-totalEmployeeWage=Working_Per_Hr*empHr;
-console.log("Totala Employee Wage :" + totalEmployeeWage);    
+const NUM_WORKING_DAYS=20;
+for(let day=0;day<NUM_WORKING_DAYS;day++)
+{
+    let empCheck=Math.floor(Math.random()*10)%3;
+    empHr=empHr+getWorkingHr(empCheck);
+}
+ totalEmployeeWage=Working_Per_Hr*empHr;
+console.log("Total Hrs :" + empHr +"     " + "EmpWage" + totalEmployeeWage);    
 
