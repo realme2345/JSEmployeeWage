44e70f4bdbb751f7c33015ae108f387c94d26b12
unknown <rajakongara2507@gmail.com>
2022-03-02 21:32:07 +0530

    [Raja]Add Validating The name using REGEX a Employee PayRoll details

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -16,20 +16,29 @@ constructor(...params)
 
 //getter and setter method
 get name(){return this._name;}
-set name(name){this._name=name;}
+set name(name){
+  let regex=RegExp("^[A-Z][A-Za-z0-9]{3,}$");
+  if(regex.test(name))
+    this._name=name;
+  else throw'Name is Incorrect';
+}
 toString()
 {
   const options={year:'numeric',month:'long',day:'numeric'};
   const empDate=this.StartDate===undefined ? "undefined":
                 this.StartDate.toLocaleDateString("en-US",options);
-  return "Id" + this.Id + ",name=" + this.name + ", salary=" + this.Salary + ", "+ "Gender=" + this.Gender
+  return "Id=" + this.Id + ",name=" + this.name + ", salary=" + this.Salary + ", "+ "Gender=" + this.Gender
               + ", startDate=" +empDate;
 }
 }
 let employeePayRoll=new EmployeePayRoll(1,30000,"Raja");
 console.log(employeePayRoll.toString());
+try{
 employeePayRoll.name="Prabha";
 console.log(employeePayRoll.toString());
+}catch(e){
+  console.log(e);
+}
 let newemployeePayRoll=new EmployeePayRoll(1,35000,"Terrasa","F" , new Date());
 console.log(newemployeePayRoll.toString());
 
