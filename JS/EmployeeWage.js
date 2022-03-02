f447e5fd7f1ca6ddcb8e599459d598cf1fe3972d
unknown <rajakongara2507@gmail.com>
2022-03-02 21:13:16 +0530

    [Raja]Add Date And Gender a Employee PayRoll details

--- JS/EmployeeWage.js
+++ JS/EmployeeWage.js
@@ -3,11 +3,15 @@ class EmployeePayRoll
 {
   Id;
   Salary;
-constructor(id,salary,name)
+  Gender;
+  StartDate;
+constructor(...params)
 {
-  this.Id=id;
-  this.Salary=salary;
-  this.name=name;
+  this.Id=params[0];
+  this.Salary=params[1];
+  this.name=params[2];
+  this.Gender=params[3];
+  this.StartDate=params[4];
 }
 
 //getter and setter method
@@ -15,11 +19,18 @@ get name(){return this._name;}
 set name(name){this._name=name;}
 toString()
 {
-  return "id =" + this.Id + ", name = " + this.name + ", salary = " + this.Salary;
+  const options={year:'numeric',month:'long',day:'numeric'};
+  const empDate=this.StartDate===undefined ? "undefined":
+                this.StartDate.toLocaleDateString("en-US",options);
+  return "Id" + this.Id + ",name=" + this.name + ", salary=" + this.Salary + ", "+ "Gender=" + this.Gender
+              + ", startDate=" +empDate;
 }
 }
 let employeePayRoll=new EmployeePayRoll(1,30000,"Raja");
 console.log(employeePayRoll.toString());
 employeePayRoll.name="Prabha";
 console.log(employeePayRoll.toString());
+let newemployeePayRoll=new EmployeePayRoll(1,35000,"Terrasa","F" , new Date());
+console.log(newemployeePayRoll.toString());
+
 
